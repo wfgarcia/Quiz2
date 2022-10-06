21b75dff07b7eb637afd8dc877ee7bd8f9d3f314
wfgarcia <wfgarcia@cpp.edu>
2022-10-05 21:01:57 -0700

    Added 'starting screen'

---
+++ TicTacToe.c
@@ -0,0 +1,42 @@
+//Tic Tac Toe game between 2 players or a computer
+#include<stdio.h>
+
+//global var
+
+int main (){
+    //create the starting variables and the enviornment of the game
+    printf("============================\n\n");
+    printf("      TIC TAC TOE           \n\n");
+    printf("1 - Player vs. Player\n\n");
+    printf("2 - Player vs. Computer\n\n");
+
+
+    //prompt user for the game they want to play
+
+
+    //read information from console
+
+
+    //create the playing field and initialize players
+
+
+    //play the game and loop for the amount of actions 
+
+
+    //decide winner 
+
+
+    // option to play again 
+
+
+    //utilize multiple functions in order to loop the game
+
+
+    //function for computer lop
+
+
+    //ending print statement
+
+
+    return 0; 
+}
