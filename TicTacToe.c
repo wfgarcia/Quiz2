80c5f14c0f6efce153f0c0cc03fb0527e717d55d
wfgarcia <wfgarcia@cpp.edu>
2022-10-05 21:38:20 -0700

    Rules  Function defined and pv set up

--- TicTacToe.c
+++ TicTacToe.c
@@ -2,20 +2,29 @@
 #include<stdio.h>
 
 //global var
+    int winner;
+    
+void rules();
+void pvp();
+void pvpc();
 
 int main (){
+    char choice; 
     //create the starting variables and the enviornment of the game
     printf("============================\n\n");
     printf("      TIC TAC TOE           \n\n");
-    printf("1 - Player vs. Player\n\n");
-    printf("2 - Player vs. Computer\n\n");
-
+    printf("A - Player vs. Player\n\n");
+    printf("B - Player vs. Computer\n\n");
+    
 
     //prompt user for the game they want to play
-
+    printf("Enter your choice (A or B): ");
 
     //read information from console
-
+    choice = getchar();
+    if(choice == 'A'){
+        pvp();
+    }
 
     //create the playing field and initialize players
 
@@ -36,7 +45,25 @@ int main (){
 
 
     //ending print statement
-
+    
 
     return 0; 
+}
+
+void rules(){
+    printf("Enter the coordinates according to the feild below\n\n");
+    printf("+-----------------+\n");
+    printf("|(1,1)|(1,2)|(1,3)|\n");
+    printf("|(2,1)|(2,2)|(2,3)|\n");
+    printf("|(3,1)|(3,2)|(3,3)|\n");
+    printf("+-----------------+\n");
+}
+
+void pvp(){
+    rules();
+    
+}
+
+void pvpc(){
+    rules();
 }
