ffcc83127d1250d2d2d693c02acc4e651fae22e8
wfgarcia <wfgarcia@cpp.edu>
2022-10-06 12:28:58 -0700

    Pvp beta

--- TicTacToe.c
+++ TicTacToe.c
@@ -1,14 +1,148 @@
 //Tic Tac Toe game between 2 players or a computer
 #include<stdio.h>
+#include<stdbool.h>
 
 //global var
-int winner;
+int winner = 0;
 int playSpace[3][3]= {{1,2,3},{4,5,6},{7,8,9}};
+char board[9];
+bool player1 = true;
+bool player2 = false;
+bool computer = false;
+int turns = 1;
+bool win;
     
-void rules();
-void pvp();
-void pvpc();
-void wincon();
+void rules(){
+    printf("\n\nEnter the coordinates according to the feild below\n\n");
+    printf("+-----------------+\n");
+    printf("|  1  |  2  |  3  |\n");
+    printf("|  4  |  5  |  6  |\n");
+    printf("|  7  |  8  |  9  |\n");
+    printf("+-----------------+\n");
+}
+
+void currentBoard(){
+     printf("+-----------------+\n");
+    printf("|  %C  |  %C  |  %C  |\n", board[0], board[1], board[2]);
+    printf("|  %C  |  %C  |  %C  |\n", board[3], board[4], board[5]);
+    printf("|  %C  |  %C  |  %C  |\n", board[6], board[7], board[8]);
+    printf("+-----------------+\n");
+}
+void setBoardP1(int move){
+if(move == 1){
+    board[0] = 'O';
+}else if(move == 2){
+    board[1] = 'O';
+}else if(move == 3){
+    board[2] = 'O';
+}else if(move == 4){
+    board[3] = 'O';
+}else if(move == 5){
+    board[4] = 'O';
+}else if(move == 6){
+    board[5] = 'O';
+}else if(move == 7){
+    board[6] = 'O';
+}else if(move == 8){
+    board[7] = 'O';
+}else if(move == 9){
+    board[8] = 'O';
+}
+}
+
+void setBoardP2(int move){
+if(move == 1){
+    board[0] = 'X';
+}else if(move == 2){
+    board[1] = 'X';
+}else if(move == 3){
+    board[2] = 'X';
+}else if(move == 4){
+    board[3] = 'X';
+}else if(move == 5){
+    board[4] = 'X';
+}else if(move == 6){
+    board[5] = 'X';
+}else if(move == 7){
+    board[6] = 'X';
+}else if(move == 8){
+    board[7] = 'X';
+}else if(move == 9){
+    board[8] = 'X';
+}
+}
+
+bool checkWin(char board[]){
+    if(board[0] && board[1] && board[2] == 'O' || 'X'){
+        return true;
+    }else if(board[3] && board[4] && board[5] == 'O' || 'X'){
+        return true;
+    }else if(board[6] && board[7] && board[8] == 'O' || 'X'){
+        return true;
+    }
+
+    if(board[0] && board[3] && board[6] == 'O' || 'X'){
+        return true;
+    }else if(board[1] && board[4] && board[7] == 'O' || 'X'){
+        return true;
+    }else if(board[2] && board[5] && board[8] == 'O' || 'X'){
+        return true;
+    }
+
+    if(board[0] && board[4] && board[8] == 'O' || 'X'){
+        return true;
+    }else if(board[6] && board[4] && board[2] == 'O' || 'X'){
+        return true;
+    }
+}
+
+void pvp(){
+    int move;
+    bool check;
+    rules();
+    while(turns < 10 || winner == 0){
+        if(turns = 1||3||5||7||9){
+            printf("Player 1 pick a space(1-9): ");
+            scanf("%d", &move);
+            if (board[move - 1] > 0){
+                printf("Sorry this space is taken try again\n\n");
+            }else{
+                setBoardP1(move);
+                turns ++;
+                check = checkwinner(board)
+                if(check){
+                    winner = 1;
+                } 
+                }
+                currentBoard();
+        }
+
+        if(turns = 2||4||6||8){
+            printf("Player 2 pick a space(1-9): ");
+            scanf("%d", &move);
+            if (board[move - 1] > 0){
+                printf("Sorry this space is taken try again\n\n");
+            }else{
+                setBoardP2(move);
+                turns ++;
+                check = checkwinner(board)
+                if(check){
+                    winner = 2;
+                } 
+                }
+                currentBoard();    
+        }
+        
+        
+    }
+    
+}
+
+void pvpc(){
+    rules();
+}
+
+
 
 int main (){
     char choice; 
@@ -26,6 +160,8 @@ int main (){
     choice = getchar();
     if(choice == 'A'){
         pvp();
+    }else if (choice == 'B'){
+        pvpc();
     }
 
     //create the playing field and initialize players
@@ -35,7 +171,13 @@ int main (){
 
 
     //decide winner 
-
+    if (winner == 1){
+        printf("Player 1 has won!");
+    }else if(winner == 2){
+        printf("Player 2 has won!");
+    }else {
+        printf("Draw");
+    }
 
     // option to play again? 
 
@@ -52,26 +194,3 @@ int main (){
     return 0; 
 }
 
-void rules(){
-    printf("\n\nEnter the coordinates according to the feild below\n\n");
-    printf("+-----------------+\n");
-    printf("|  1  |  2  |  3  |\n");
-    printf("|  4  |  5  |  6  |\n");
-    printf("|  7  |  8  |  9  |\n");
-    printf("+-----------------+\n");
-}
-
-void pvp(){
-    rules();
-    
-}
-
-void pvpc(){
-    rules();
-}
-
-void wincon(){
-    if( 1 == 1){
-
-    }
-}
